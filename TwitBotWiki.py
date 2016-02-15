452761c65a21734afc31bc7b55dee88f01a9b6bb
Harrison Lingren <harrisonlingren@gmail.com>
2016-02-15 10:49:52 -0500

    Added error handling
    Added error handling for Disambiguation and Index errors.

--- TwitBotWiki.py
+++ TwitBotWiki.py
@@ -13,26 +13,35 @@ def TwitBotWiki(tweet):
 	tweet_id = parsedJSON['id_str']
 	tweet_text = parsedJSON['text']
 	
-	term = re.findall('"([^"]*)"', tweet_text)
-	print('>  @'+tweet_user+': '+tweet_text+' | term: '+term[0])
-	
-	reply = ('@' + tweet_user + wikiToString(term))
-	replyASCII = str(unicodedata.normalize('NFKD', reply).encode('ascii','ignore'))
+	try:
+		term = re.findall('"([^"]*)"', tweet_text)
+		print('>  @'+tweet_user+': '+tweet_text+' | term: '+term[0])
+		
+		reply = ('@' + tweet_user + wikiToString(term[0]))
+		replyASCII = str(unicodedata.normalize('NFKD', reply).encode('ascii','ignore'))
+	except IndexError:
+		print('>  @'+tweet_user+': '+tweet_text+' | IndexError, no term found')
+		reply = ('@' + tweet_user + ' could not find "'+term[0]+'"! Try again.')
+		replyASCII = str(unicodedata.normalize('NFKD', reply).encode('ascii','ignore'))
+		pass
 	
 	print("  >  " + replyASCII,'\n')
-	
 	api.update_status(reply, in_reply_to_status_id = tweet_id)
 	return 'Tweet Sent!'
 	
+	
 def wikiToString(term):
 	
-	wikiPage = wikipedia.page(wikipedia.search(term)[0])
-	wikiURL = shortURL(wikiPage.url)
-	wikiSummary = wikiPage.summary
-	summ = (wikiSummary[0:79])
-	
-	outp = (' ' + summ + '... Read more: ' + wikiURL)
-	
+	try:
+		wikiPage = wikipedia.page(wikipedia.search(term)[0])
+		wikiURL = shortURL(wikiPage.url)
+		wikiSummary = wikiPage.summary
+		summ = (wikiSummary[0:79])
+		outp = (' ' + summ + '... Read more: ' + wikiURL)
+	except wikipedia.exceptions.DisambiguationError:
+		outp = ' Wiki article for "'+term+'" is a disambiguation! Try being more specific.'
+		pass
+		
 	return outp
 	
 def shortURL(url):
