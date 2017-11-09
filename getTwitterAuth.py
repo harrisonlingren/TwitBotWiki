4eb7831f82440e7ad5b784919271a2d2e62d8ccf
Harrison Lingren <harrisonlingren@gmail.com>
2017-11-09 19:33:23 +0000

    change to environment variables

--- getTwitterAuth.py
+++ getTwitterAuth.py
@@ -1,8 +1,12 @@
-import tweepy
+import tweepy, os
 
 def getTwitterAuth():
+        c_k = os.environ.get('consumer_key')
+        c_s = os.environ.get('consumer_secret')
+        a_t = os.environ.get('access_token')
+        a_t_s = os.environ.get('access_token_secret')
 
-        auth = tweepy.OAuthHandler('consumer_key', 'consumer_secret')
-        auth.access_token = 'access_token'
-        auth.access_token_secret = 'access_token_secret'
+        auth = tweepy.OAuthHandler(c_k, c_s)
+        auth.access_token = a_t
+        auth.access_token_secret = a_t_s
         return auth
